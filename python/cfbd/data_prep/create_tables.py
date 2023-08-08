caf4886ec6e75e9e8f696399241139722d63a866
benscheese30 <ben.scheese@outlook.com>
2023-08-07 21:55:40 -0600

    updates to get game data into db

--- python/cfbd/data_prep/create_tables.py
+++ python/cfbd/data_prep/create_tables.py
@@ -3,7 +3,6 @@ import py.cfbd as cfbd
 import datetime
 
 current_year = datetime.datetime.now().year
-year = 2003
 
 # getting games endpoint requirements
 endpoint = "/games"
@@ -23,28 +22,26 @@ games_sql = """
         conference_game boolean,
         attendance integer,
         venue_id integer,
-        venue varchar(100),
+        venue varchar(150),
         home_id integer,
-        home_team varchar(100),
-        home_conference varchar(20),
-        home_division varchar(20),
+        home_team varchar(150),
+        home_conference varchar(50),
+        home_division varchar(50),
         home_points integer,
         home_line_scores integer[],
         home_post_win_prob float,
         home_pregame_elo float,
         home_postgame_elo float,
         away_id integer,
-        away_team varchar(100),
-        away_conference varchar(20),
-        away_division varchar(20),
+        away_team varchar(150),
+        away_conference varchar(50),
+        away_division varchar(50),
         away_points integer,
         away_line_scores integer[],
         away_post_win_prob float,
         away_pregame_elo float,
         away_postgame_elo float,
-        excitement_index float,
-        highlights integer[],
-        notes varchar(1000)
+        excitement_index float
 )        
 """
 
@@ -52,14 +49,14 @@ games_sql = """
 dbs.postgres_create_table(games_sql)
 
 # insert data into postgres
-for year in range(2003, current_year + 1):
+for year in range(1869, current_year + 1):
     print(year)
     games_params = {"year": year}
     df = cfbd.get_data(endpoint, games_params)
-    if df is not None:
-        dbs.data_insert("games", df)
-
-for row in df:
-    print(row["id"])
+    # remove highlights and notes keys from dictionary
+    for d in df:
+        d.pop("highlights", None)
+        d.pop("notes", None)
+    # insert data into postgres
     if df is not None:
         dbs.data_insert("games", df)
