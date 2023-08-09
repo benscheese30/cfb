07549859d515edb82bbffd6e79fc0142822916a2
benscheese30 <ben.scheese@outlook.com>
2023-08-08 23:33:51 -0600

    fixed errors in code for function

--- python/60_in_60/selenium_60in60.py
+++ python/60_in_60/selenium_60in60.py
@@ -2,15 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import py.dbs as dbs
 
-# setting up driver
-driver = webdriver.Chrome()
 urls = {
-    "2022": "https://kslsports.com/487728/hans-scotty-60-in-60-list/",
-    "2023": "https://kslsports.com/502045/60-in-60-best-college-football-players-in-utah/"
+    2022: "https://kslsports.com/487728/hans-scotty-60-in-60-list/",
+    2023: "https://kslsports.com/502045/60-in-60-best-college-football-players-in-utah/"
 }
 
-def get_60in60_list(year, url):
-    # connectig to url
+def pull_60in60_list(year, url):
+    # setting up driver
+    driver = webdriver.Chrome()
+
+    # connecting to url
     driver.get(url)
 
     # getting list of players
@@ -21,10 +22,13 @@ def get_60in60_list(year, url):
     # and only if team names are in the list ("BYU", "Utah", "USU", "Weber", "SUU")
     player_names = []
     for p in paragraph_elements:
-        if "BYU" in p.text or "Utah" in p.text or "USU" in p.text or "Weber" in p.text or "SUU" in p.text:
-            # if p also contains a number (1-60), then it is a player name
-            if any(str(i) in p.text for i in range(1, 61)):
-                player_names.append(p.text)
+     if "BYU" in p.text or "Utah" in p.text or "USU" in p.text or "Weber" in p.text or "SUU" in p.text:
+         # if p also contains a number (1-60), then it is a player name
+         if any(str(i) in p.text for i in range(1, 61)):
+             player_names.append(p.text)
+
+    # closing connection to url
+    driver.close()
 
     # excluding elements from player_names that have more than 50 characters
     player_names = [p for p in player_names if len(p) < 50]
@@ -32,36 +36,26 @@ def get_60in60_list(year, url):
     # preparing data for database
     player_list = []
     for p in player_names:
-        # setting team name to "Weber State" if team name is "Weber"
-        if p.split("(")[1].split(")")[0].strip() == "Weber":
-            team_name = "Weber State"
-        elif p.split("(")[1].split(")")[0].strip() == "USU":
-            team_name = "Utah State"
-        else:
-            team_name = p.split("(")[1].split(")")[0].strip()
+     # setting team name to "Weber State" if team name is "Weber"
+     if p.split("(")[1].split(")")[0].strip() == "Weber":
+         team_name = "Weber State"
+     elif p.split("(")[1].split(")")[0].strip() == "USU":
+         team_name = "Utah State"
+     else:
+         team_name = p.split("(")[1].split(")")[0].strip()
 
-        player_list.append({
-            "name": p.split(".")[1].split("(")[0].strip(),
-            "team": team_name,
-            "rank": p.split(".")[0].strip().split(" ")[-1],
-            "year": year
-        })
+     player_list.append({
+         "name": p.split(".")[1].split("(")[0].strip(),
+         "team": team_name,
+         "rank": p.split(".")[0].strip().split(" ")[-1],
+         "year": year
+     })
 
     # inserting data into postgres
     dbs.data_insert("cfb_60in60", player_list)
 
-# creating table in postgres
-sql = """
-    create table cfb.cfb_60in60 (
-        id serial primary key,
-        name varchar(100),
-        team varchar(100),
-        rank integer,
-        year integer
-    )
-"""
-dbs.postgres_create_table(sql)
+    return player_list
 
-# inserting data into postgres
+# get player list for each year
 for year, url in urls.items():
-    get_60in60_list(year, url)
+    lst = pull_60in60_list(year, url)
