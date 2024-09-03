5e8169dc487ed06927a0aa8815129d0438a74a71
benscheese30 <ben.scheese@outlook.com>
2024-09-03 07:14:20 -0600

    updated script to pull through all future games

--- pipelines/web_scraping/espn/game_predictions/pull_game_predictions.py
+++ pipelines/web_scraping/espn/game_predictions/pull_game_predictions.py
@@ -11,43 +11,48 @@ games = cfbd.get_data(
     endpoint="/games",
     params={
         "year": 2024,
-        "division": "fbs",
-        "team": "Utah"
+        "division": "fbs"
     }
 )
 
-# saving as a set to avoid duplicates
-game_ids = (game["id"] for game in games)
-game_ids = list(game_ids)
+games_dict = {}
+for g in games:
+    week = g["week"]
+    game_id = g["id"]
+    
+    if week > 1:
+        if week in games_dict:
+            games_dict[week].append(game_id)
+        else:
+            games_dict[week] = [game_id]
 
-for index, game_id in enumerate(game_ids):
-    if index < 10:
-        print(f"Game ID: {game_id}")
 
-# TODO: make this loop more scalable by adding a retry or a dynamic list to retry on
-fpi_game_predictions = []
-retry_list = []
-for index, game_id in enumerate(retry_list):
-    print(index + 1)
-    try:
-        with get_webdriver() as driver:
-            driver.get(f"https://www.espn.com/college-football/game/_/gameId/{game_id}")
+fpi_game_predictions = {} # Initialize retry_list with all game_ids
+for w in games_dict:
+    print(f"Processing Week: {w}")
+    game_ids = games_dict[w]
+    retry_list = game_ids.copy()
 
-            wait = WebDriverWait(driver, 15)
-            game_prediction = wait.until(
-                EC.presence_of_element_located((By.CLASS_NAME, "matchupPredictor"))
-            )
+    while retry_list:
+        game_id = retry_list.pop(0)  # Get the first game_id from retry_list
 
-            preds = game_prediction.text.split('\n')
+        try:
+            with get_webdriver() as driver:
+                driver.get(f"https://www.espn.com/college-football/game/_/gameId/{game_id}")
 
-            fpi_game_predictions.append({
-                "game_id": game_id,
-                "home_team": preds[2].float(),
-                "away_team": preds[0].float(),
-            })
-    except Exception as e:
-        retry_list.append(game_id)
+                wait = WebDriverWait(driver, 15)
+                game_prediction = wait.until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "matchupPredictor"))
+                )
 
+                preds = game_prediction.text.split('\n')
+
+                fpi_game_predictions[game_id] = [float(preds[2]), float(preds[0])]
+        except Exception as e:
+            print(f"Error processing Game ID: {game_id}")
+            retry_list.append(game_id)  # Add the game_id back to retry_list for retrying
+
+print("All game_ids processed successfully.")
 
 s3 = aws.aws_client(
     aws.aws_config(service="s3", localstack=True)
