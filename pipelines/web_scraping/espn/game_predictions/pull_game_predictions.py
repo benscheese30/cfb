c9461209ed96dc9f1cba3277df772364323e6192
benscheese30 <ben.scheese@outlook.com>
2024-09-03 07:43:07 -0600

    created multi thread approach

--- pipelines/web_scraping/espn/game_predictions/pull_game_predictions.py
+++ pipelines/web_scraping/espn/game_predictions/pull_game_predictions.py
@@ -5,6 +5,7 @@ from utils.extract.web_scraping import get_webdriver
 from utils import cfbd, aws
 from io import BytesIO
 import json
+import threading
 
 # getting a list of game ids for the season
 games = cfbd.get_data(
@@ -28,6 +29,25 @@ for g in games:
 
 
 fpi_game_predictions = {} # Initialize retry_list with all game_ids
+
+def process_game(game_id):
+    try:
+        with get_webdriver() as driver:
+            driver.get(f"https://www.espn.com/college-football/game/_/gameId/{game_id}")
+
+            wait = WebDriverWait(driver, 15)
+            game_prediction = wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, "matchupPredictor"))
+            )
+
+            preds = game_prediction.text.split('\n')
+
+            fpi_game_predictions[game_id] = [float(preds[2]), float(preds[0])]
+    except Exception as e:
+        print(f"Error processing Game ID: {game_id}")
+        retry_list.append(game_id)  # Add the game_id back to retry_list for retrying
+
+threads = []
 for w, game_ids in games_dict.items():
     print(f"Processing Week: {w}")
     retry_list = game_ids.copy()
@@ -35,21 +55,14 @@ for w, game_ids in games_dict.items():
     while retry_list:
         game_id = retry_list.pop(0)  # Get the first game_id from retry_list
 
-        try:
-            with get_webdriver() as driver:
-                driver.get(f"https://www.espn.com/college-football/game/_/gameId/{game_id}")
+        t = threading.Thread(target=process_game, args=(game_id,))
+        threads.append(t)
+        t.start()
 
-                wait = WebDriverWait(driver, 15)
-                game_prediction = wait.until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "matchupPredictor"))
-                )
+# Wait for all threads to complete
+for t in threads:
+    t.join()
 
-                preds = game_prediction.text.split('\n')
-
-                fpi_game_predictions[game_id] = [float(preds[2]), float(preds[0])]
-        except Exception as e:
-            print(f"Error processing Game ID: {game_id}")
-            retry_list.append(game_id)  # Add the game_id back to retry_list for retrying
 
 print("All game_ids processed successfully.")
 
@@ -60,7 +73,7 @@ s3 = aws.aws_client(
 s3.upload_fileobj(
     Fileobj=BytesIO(json.dumps(fpi_game_predictions).encode()),
     Bucket="college-football",
-    Key="data/espn/fpi_game_predictions/2024_week_2l.json"
+    Key="data/espn/fpi_game_predictions/2024_week_2.json"
 )
 
 
